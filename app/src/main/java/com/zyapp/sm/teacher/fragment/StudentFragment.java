c74b606a229964d5d4faa991d663440e1bde08e0
ZhongYang <2812913272@qq.com>
2020-07-06 20:46:01 +0800

    向贵龙提交了一条数据

---
+++ app/src/main/java/com/zyapp/sm/teacher/fragment/StudentFragment.java
@@ -0,0 +1,21 @@
+package com.zyapp.sm.teacher.fragment;
+
+import android.widget.BaseAdapter;
+
+import com.zyapp.sm.R;
+import com.zyapp.sm.teacher.base.BaseFragment;
+
+/**
+ * @项目名称 Student-Management
+ * @包名 com.zyapp.sm.teacher.fragment
+ * @作者 钟阳
+ * @类名 StudentFragment
+ * @日期 2020/7/6 19:12
+ * @描述 TODO
+ */
+public class StudentFragment extends BaseFragment {
+    @Override
+    protected int getRootViewResId() {
+        return R.layout.fragment_student;
+    }
+}
