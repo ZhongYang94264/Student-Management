4a52bfd71f2269cc6d7d64189ca186e93e84d0a7
ZhongYang <2812913272@qq.com>
2020-07-06 22:16:06 +0800

    xgl update activity

---
+++ app/src/main/java/com/zyapp/sm/activity/AdminRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.zyapp.sm.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zyapp.sm.R;
+
+public class AdminRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_register);
+    }
+}
---
+++ app/src/main/java/com/zyapp/sm/activity/TeacherRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.zyapp.sm.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zyapp.sm.R;
+
+public class TeacherRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_register);
+    }
+}
