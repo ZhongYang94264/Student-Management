68faaef7a307be8d74d9729e4ce51dd42c076ac3
ZhongYang <2812913272@qq.com>
2020-07-07 14:01:53 +0800

    update of peice a date to xgl

--- app/src/main/java/com/zyapp/sm/activity/AdminLoginActivity.java
+++ app/src/main/java/com/zyapp/sm/activity/AdminLoginActivity.java
@@ -6,14 +6,17 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.zyapp.sm.R;
+import com.zyapp.sm.admin.AdministratorsActivity;
 
 
 public class AdminLoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv_register;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +37,22 @@ public class AdminLoginActivity extends AppCompatActivity implements View.OnClic
      */
     private void initView() {
         tv_register = findViewById(R.id.tv_register);
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
         tv_register.setOnClickListener(this);
     }
 
     /**
      * 实现监听接口
+     *
      * @param v
      */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn_login:
+                startActivity(new Intent(AdminLoginActivity.this, AdministratorsActivity.class));
+                break;
             case R.id.tv_register:
                 startActivity(new Intent(AdminLoginActivity.this, AdminRegisterActivity.class));
 
