e356503371163455b5bcd3d7d5e7cf6f45720f31
HaiYan <2859455572@qq.com>
2020-07-07 14:29:24 +0800

    lhy  update

--- app/src/main/java/com/zyapp/sm/activity/AdminRegisterActivity.java
+++ app/src/main/java/com/zyapp/sm/activity/AdminRegisterActivity.java
@@ -1,6 +1,9 @@
 package com.zyapp.sm.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -8,6 +11,9 @@ import androidx.core.content.ContextCompat;
 import com.zyapp.sm.R;
 
 public class AdminRegisterActivity extends AppCompatActivity {
+    EditText et_register_code, et_admin_account_num, et_passwordone, et_confirm_password;//注册码，管理员账号， 密码，确定密码
+
+    Button btn_admin_register;//注册按钮
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,17 @@ public class AdminRegisterActivity extends AppCompatActivity {
         }
         //设置状态栏颜色
         this.getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorAdmin));
+
+        initadminRegister();
+    }
+
+
+    //初始化
+    public void initadminRegister() {
+        et_register_code = findViewById(R.id.et_register_code);
+        et_admin_account_num = findViewById(R.id.et_admin_account_num);
+        et_passwordone = findViewById(R.id.et_passwordone);
+        et_confirm_password= findViewById(R.id.et_confirm_password);
+        btn_admin_register = findViewById(R.id.btn_admin_register);
     }
 }
--- app/src/main/java/com/zyapp/sm/activity/TeacherRegisterActivity.java
+++ app/src/main/java/com/zyapp/sm/activity/TeacherRegisterActivity.java
@@ -1,6 +1,8 @@
 package com.zyapp.sm.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -8,7 +10,8 @@ import androidx.core.content.ContextCompat;
 import com.zyapp.sm.R;
 
 public class TeacherRegisterActivity extends AppCompatActivity {
-
+    EditText  et_register_name,et_teacher_account_num,et_password,et_work_number;//姓名  教师工号  密码  确认密码
+    Button btn_teacher_register;//教师按钮
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +22,15 @@ public class TeacherRegisterActivity extends AppCompatActivity {
         }
         //设置状态栏颜色
         this.getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorTeacher));
+        initteacherRegister();
+    }
+
+    //初始化
+    public void initteacherRegister() {
+        et_register_name = findViewById(R.id.et_register_name);
+        et_teacher_account_num = findViewById(R.id.et_teacher_account_num);
+        et_password = findViewById(R.id.et_password);
+        et_work_number = findViewById(R.id.et_work_number);
+        btn_teacher_register = findViewById(R.id.btn_teacher_register);
     }
 }
