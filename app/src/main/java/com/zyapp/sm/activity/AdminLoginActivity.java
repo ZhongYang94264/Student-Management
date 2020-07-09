3f5ce1ecc69000acaa972bc3982ae9be3e33d989
LuoGongZi <luo2307876709@vip.qq.com>
2020-07-09 16:08:21 +0800

    lkf

--- app/src/main/java/com/zyapp/sm/activity/AdminLoginActivity.java
+++ app/src/main/java/com/zyapp/sm/activity/AdminLoginActivity.java
@@ -4,19 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.zyapp.sm.R;
 import com.zyapp.sm.admin.AdministratorsActivity;
+import com.zyapp.sm.sql.DBOperate;
+import com.zyapp.sm.sql.sqlData;
 
 
 public class AdminLoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv_register;
     private Button btn_login;
+    private EditText et_login,et_psaa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,18 @@ public class AdminLoginActivity extends AppCompatActivity implements View.OnClic
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_admin_login:
+                String login = et_login.getText().toString();
+                String pass = et_psaa.getText().toString();
+                if ("".equals(login)||"".equals(pass)){
+                    Toast.makeText(this, "用户名或密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //访问数据库
+                DBOperate dbOperate = new DBOperate();
+                dbOperate.OpenDB(AdminLoginActivity.this);
+                //调用查询方法，判断用户是否存在
+                //创建查询语句
+                String login_sql = "select * from" + sqlData. + "where _id = '" + login + "'";
                 startActivity(new Intent(AdminLoginActivity.this, AdministratorsActivity.class));
                 break;
             case R.id.tv_register:
