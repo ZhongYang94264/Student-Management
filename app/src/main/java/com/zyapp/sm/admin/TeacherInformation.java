f2fb22ec4b0405ff1e7c703126ca69ea686ecf7f
XieLi <1323219644@qq.com>
2020-07-09 18:58:57 +0800

    xl

--- app/src/main/java/com/zyapp/sm/admin/TeacherInformation.java
+++ app/src/main/java/com/zyapp/sm/admin/TeacherInformation.java
@@ -10,5 +10,6 @@ public class TeacherInformation extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_information);
+
     }
 }
